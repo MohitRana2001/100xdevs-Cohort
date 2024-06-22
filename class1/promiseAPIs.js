f8c3e4244e40cdc2ca5b32cb325b4f499ff5f733
MOHIT RANA <awesomemohit20@gmail.com>
2024-06-22 11:02:37 +0530

    Learning APIs

---
+++ class1/promiseAPIs.js
@@ -0,0 +1,6 @@
+//Promise.all :- waits for all the promises to get finished or resolved.
+//Promise.allSettled :- waits for all the promises to get settled (either resolved or rejected) (returns the values of promises in array).
+//Promise.any :- waits for any of the promises to get resolved. (If all promises fail, then it will result an array of all errors) - (Aggregated errors).
+//Promise.race :- waits for any of the promises to get settled (either resolved or rejected)
+//Promise.resolve :- returns a resolved promise.
+//Promise.reject :- returns a rejected promise.
