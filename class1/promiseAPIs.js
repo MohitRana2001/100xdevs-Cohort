3e4494343971124fb0b1f5005d63264edaeaf8f9
MOHIT RANA <awesomemohit20@gmail.com>
2024-06-22 11:28:22 +0530

    code examples for APIs

--- class1/promiseAPIs.js
+++ class1/promiseAPIs.js
@@ -4,3 +4,46 @@
 //Promise.race :- waits for any of the promises to get settled (either resolved or rejected)
 //Promise.resolve :- returns a resolved promise.
 //Promise.reject :- returns a rejected promise.
+
+//code example for Promise.all
+
+const p1 = new Promise((resolve,reject) => {
+    setTimeout(() => reject("p1 failed"), 3000);
+});
+
+const p2 = new Promise((resolve,reject) => {
+    setTimeout(() => reject("p2 failed"), 5000);
+});
+
+const p3 = new Promise((resolve,reject) => {
+    setTimeout(() => reject("p3 failed"), 2000);
+});
+
+// Promise.all([p1,p2,p3]).then(res => {
+//     console.log(res); //output: ["p1 success", "p2 success", "p
+// })
+// .catch((err) => {
+//     console.error(err); //output: "p2 failed"
+// });
+
+// Promise.allSettled([p1,p2,p3]).then(res => {
+//     console.log(res); //output: ["p1 success", "p2 success", "p
+// })
+// .catch((err) => {
+//     console.error(err); //output: "p2 failed"
+// });
+
+// Promise.race([p1,p2,p3]).then(res => {
+//     console.log(res); //output: ["p1 success", "p2 success", "p
+// })
+// .catch((err) => {
+//     console.error(err); //output: "p2 failed"
+// });
+
+Promise.any([p1,p2,p3]).then(res => {
+    console.log(res); //output: ["p1 success", "p2 success", "p
+})
+.catch((err) => {
+    console.error(err); //output: "p2 failed"
+    console.log(err.errors);
+});
