273fde2df46f1e33e4a1789b875a1e12277a096c
MOHIT RANA <awesomemohit20@gmail.com>
2024-06-30 23:05:56 +0530

    Solved the question

--- week-2/01-async-js/hard (promises)/3-promise-all.js
+++ week-2/01-async-js/hard (promises)/3-promise-all.js
@@ -5,19 +5,52 @@
  */
 
 function wait1(t) {
-
+    return new Promise((resolve, reject) => {
+        setTimeout(() => resolve(`wait1 resolved after ${t} seconds`), t * 1000);
+    });
 }
 
 function wait2(t) {
-
+    return new Promise((resolve, reject) => {
+        setTimeout(() => resolve(`wait2 resolved after ${t} seconds`), t * 1000);
+    });
 }
 
 function wait3(t) {
-
+    return new Promise((resolve, reject) => {
+        setTimeout(() => resolve(`wait3 resolved after ${t} seconds`), t * 1000);
+    });
 }
 
-function calculateTime(t1, t2, t3) {
+function performOperation() {
+    // Start time
+    const startTime = performance.now();
+    
+    function calculateTime(t1, t2, t3) {
+        return Promise.all([
+            wait1(t1),
+            wait2(t2),
+            wait3(t3)
+        ]).then(results => {
+            // End time after all promises resolve
+            const endTime = performance.now();
 
+            // Calculate duration in milliseconds
+            const duration = endTime - startTime;
+
+            console.log(`Operation took ${duration} milliseconds to complete.`);
+            console.log(results); // Log results of each promise
+            return duration; // Return the time taken in milliseconds
+        });
+    }
+
+    // Call calculateTime with desired times (1 second for wait1, 2 seconds for wait2, 3 seconds for wait3)
+    calculateTime(1, 2, 3).then(totalTime => {
+        console.log(`Total time taken: ${totalTime} milliseconds.`);
+    }).catch(error => {
+        console.error('Error:', error);
+    });
 }
 
-module.exports = calculateTime;
+// Call the function to perform the operation
+performOperation();
