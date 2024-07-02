6dbd2b69a66002b3acf9fc4a03d3a16bd754dcfb
MOHIT RANA <awesomemohit20@gmail.com>
2024-07-03 00:40:09 +0530

    Solved the exercise

--- week-2/02-nodejs/fileServer.js
+++ week-2/02-nodejs/fileServer.js
@@ -17,19 +17,21 @@ const fs = require('fs');
 const path = require('path');
 const app = express();
 const port = 3000;
-app.get('/' , (req,res) => {
-  let allFiles = {};
-  fs.readdir("./files", (err,files) => {
+app.get('/files' , (req,res) => {
+  fs.readdir((path.join(__dirname , "./files")), (err,files) => {
     if(err) console.error("could not read files");
-
-    files.forEach((file, index) => {
-      allFiles[index] = file;
-    });
+    res.json(files);
   });
-  // res.write();
-  console.log(allFiles);
-  res.send(res.status(200).json(allFiles));
-})
+});
+
+app.get('/files/:filename', (req, res) => {
+  const filePath = path.join(__dirname,"./files", dirPath, req.params.filename);
+  fs.readFile(filePath,'utf-8', (err, data) => {
+    if (err) return res.status(404).send('File not found');
+    console.log(data);
+    res.status(200).send(data);
+  });
+});
 
 app.listen(port , () => {
   console.log(`Server is running on port ${port}`);
